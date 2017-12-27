0d60aecc4cdc114a089318f28da4aaececcc0074
Tom <tomjsmart@hotmail.co.uk>
2017-12-27 14:55:57 +0000

    first edit

--- bmGUI.py
+++ bmGUI.py
@@ -1,37 +1,48 @@
 import tkinter as tk
 
-LARGE_FONT = ("Verdana", 12)
+# define 'LARGE_FONT'
+LARGE_FONT = ("Verdana", 12)    
 
-class BMapp(tk.TK):
+# define class 'BMapp', inheriting 'Tk' from tkinter
+class BMapp(tk.Tk): 
     
-    def __init__(self, *args, **kwargs):
+    # intialise class
+    def __init__(self, *args, **kwargs):     
         
-        tk.TK__init__(self, *args, **kwargs)
+        # initialise inherited class 'Tk'
+        tk.Tk.__init__(self, *args, **kwargs)   
         
+        # define container
         container = tk.Frame(self)        
         container.pack(side="top", fill="both", expand = True)        
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
+        # define dictionary
         self.frames = {}
         frame = StartPage(container, self)
         self.frames[StartPage] = frame
         
         frame.grid(row=0, column=0, sticky="nsew")
         
-        self.showframe(StartPage)
-        
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+        self.show_frame(StartPage)
+    
+    # define method to show frame 'cont'    
+    def show_frame(self, controller):
+        frame = self.frames[controller]
         frame.tkraise
         
+# define 'StartPage', inheriting  'Frame' from tkinter
 class StartPage(tk.Frame):
     
+    # initialise class
     def __init__(self, parent, controller):
+        # initialise tk.Frame
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="This is the start page", font=LARGEFONT)
+        # define label for the start page
+        label = tk.Label(self, text="This is the start page", font=LARGE_FONT)
+        # pack label into window, with some padding
         label.pack(pady=10,padx=10)
         
 app = BMapp()
 app.mainloop()
-        
